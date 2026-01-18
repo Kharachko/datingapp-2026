cfc9bcb0813ec5792a522bd38bca099e47fe661b
Kharachko <kharvolodymyr@gmail.com>
2026-01-18 14:58:50 +0200

    Add DatingApp Collection with various API requests for user authentication
    - Included error handling scripts for various API responses.
    - Removed outdated database file.

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace API.DTOs;
+
+public class UserDto
+{
+    public required string ID { get; set; }
+    public required string Email { get; set; }
+    public required string DisplayName { get; set; }
+    public required string Token { get; set; }
+    public string? ImageUrl { get; set; }
+    
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration configuration) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = configuration["TokenKey"] ?? throw new Exception("Token key not found in configuration");
+        if (tokenKey.Length < 64)
+            throw new Exception("Token key is too short");
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new (ClaimTypes.Email, user.Email),
+            new (ClaimTypes.NameIdentifier, user.ID)
+        };
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(token);
+    }
+}
